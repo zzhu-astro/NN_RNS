507ba32c2515de074e672ce2ea484b6018fc3d0b
Zhenyu Zhu <zhenyu.zhu@sjtu.edu.cn>
2026-04-02 18:17:34 -0400

    ready for Pypi version, not done yet

--- src/nn_rns/__init__.py
+++ src/nn_rns/__init__.py
@@ -1,10 +1,9 @@
 """Public package interface for nn_rns."""
 
 from . import eos, units
+from ._version import __version__
 from .eos import EoSTable
 from .networks import RNSNetworks, rns_networks
-
-__version__ = "0.1.0"
 
 __all__ = [
     "EoSTable",
